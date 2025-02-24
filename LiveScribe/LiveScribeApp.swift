3c48e57ec9e911ed9df9fe06303016e5b4fb45f5
Christian Skorobogatow <christiansko@icloud.com>
2025-02-24 18:02:00 +0100

    Initial Commit

---
+++ LiveScribe/LiveScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveScribeApp.swift
+//  LiveScribe
+//
+//  Created by Christian Skorobogatow on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
